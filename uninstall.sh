b0c222fd72a179ae452d2615456de2ae279651e2
xKaMikax <122531941+xKaMikax@users.noreply.github.com>
2026-02-14 21:41:43 +0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-# ZIX UNINSTALLER
+# ZIX UNINSTALLER v1.2.5
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo -e "${RED}--- Полное удаление системы Zix ---${NC}"
+echo -e "${RED}--- УДАЛЕНИЕ СИСТЕМЫ ZIX ---${NC}"
 
-echo "Остановка служб..."
+echo "Остановка всех служб..."
 sudo systemctl stop wyoming-satellite mpd shairport-sync avahi-daemon 2>/dev/null
 sudo systemctl disable wyoming-satellite mpd shairport-sync avahi-daemon 2>/dev/null
 
-echo "Удаление файлов и конфигов..."
+echo "Удаление системных файлов и конфигураций..."
 sudo rm /etc/systemd/system/wyoming-satellite.service 2>/dev/null
 sudo rm /etc/mpd.conf 2>/dev/null
 sudo rm -rf /opt/wyoming-satellite
 sudo rm -rf /var/lib/mpd
 
+echo "Обновление демона системы..."
 sudo systemctl daemon-reload
-echo -e "${GREEN}Система Zix полностью удалена.${NC}"
+
+echo -e "${GREEN}Zix успешно и полностью удален.${NC}"
